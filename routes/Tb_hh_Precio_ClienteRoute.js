4ee7b0f8e8a61198405c7856f50ac3edd1c615be
aserosiris <loera.osiris@gmail.com>
2019-01-01 16:40:55 -0700

    funcion cambiada

--- routes/Tb_hh_Precio_ClienteRoute.js
+++ routes/Tb_hh_Precio_ClienteRoute.js
@@ -4,8 +4,8 @@ const tb_hh_precioCtrl = require('../controllers/precioClienteCtrl');
 
 
 
-router.get('/precioCliente/all', tb_hh_precioCtrl.getAllPrecios);
-router.post('/precioCliente/consulta',tb_hh_precioCtrl.getPrecio);
+router.get('/precioCliente/all', tb_hh_precioCtrl.getAllPrecioClientes);
+//router.post('/precioCliente/consulta',tb_hh_precioCtrl.getPrecio);
 
 
 module.exports = router;
