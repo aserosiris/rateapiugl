6d289b88295178bb4312f3a63be3c99fea29770d
aserosiris <loera.osiris@gmail.com>
2019-01-02 17:33:33 -0700

    cambio nuevo

--- controllers/precioClienteCtrl.js
+++ controllers/precioClienteCtrl.js
@@ -4,9 +4,18 @@ const PrecioCliente = require('../models/TB_HH_PRECIO_CLIENTE');
 
 
 exports.getAllPrecioClientes = async (req, res) => {
-    const results = await PrecioCliente.find({});
+    //const results = await PrecioCliente.find({});
 
-    return res.status(200).json({result: results});
+    //return res.status(200).json({result: results});
+
+    const results = await Precio.find(
+        { 
+                "PRC_RUTA_CLIE" : Number(11)
+            
+        })
+    
+        return res.status(200).json({result:results})
 }
+
 
 
