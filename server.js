52183e79d2f281e44f8a06268ab1ae4be0200a35
aserosiris <loera.osiris@gmail.com>
2019-01-17 16:03:17 -0700

    tabla nota venta

--- server.js
+++ server.js
@@ -61,6 +61,7 @@ const tb_hh_revolventes = require('./routes/tb_hh_revolventesRoute.js');
 const tb_hh_promos = require('./routes/tb_hh_promosRoutes.js');
 const tb_hh_carga_iniciales = require('./routes/tb_hh_carga_inicialesRoute.js')
 const tb_hh_arreglos = require('./routes/tb_hh_arreglosRoutes.js')
+const tb_hh_nota_ventas = require('./routes/tb_hh_nota_ventaRoutes.js')
 
 app.use('/api', user);
 app.use('/api', company);
@@ -74,6 +75,7 @@ app.use('/api', tb_hh_revolventes);
 app.use('/api', tb_hh_promos);
 app.use('/api', tb_hh_carga_iniciales);
 app.use('/api', tb_hh_arreglos);
+app.use('/api', tb_hh_nota_ventas);
 
 app.listen(process.env.PORT || 3000, () =>{
     console.log('server running in port 3000');
