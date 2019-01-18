469acddcc53a680b207435b8ccacbce8ef205892
aserosiris <loera.osiris@gmail.com>
2019-01-18 13:22:07 -0700

    correccion en tipo de dato

--- models/tb_hh_promos.js
+++ models/tb_hh_promos.js
@@ -5,7 +5,7 @@ const promosSchema = mongoose.Schema({
     PM_CLAVE_PRODUCTO: {type: Number},
     PM_CANTIDAD: {type: Number},
     PM_PRECIOXUNIDAD_PROMO: {type: Number},
-    PM_APLICAR_RUTAS: {type: Number},
+    PM_APLICAR_RUTAS: {type: String},
     PM_ESTATUS: {type: String},
     PM_EMPRESA: {type: Number}
 });
