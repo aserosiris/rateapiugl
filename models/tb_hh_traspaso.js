d0a6529d22b4642dc32fb7f8a7c8d46ad7f96afa
aserosiris <loera.osiris@gmail.com>
2019-03-13 09:28:29 -0600

    cambio de campos

--- models/tb_hh_traspaso.js
+++ models/tb_hh_traspaso.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 
 const traspasoSchema = mongoose.Schema({
     TR_FECHA:{type: Date},
-    TR_NUMERO: {type: Number},
     TR_RUTA:{type: Number},
     TR_CLAVE:{type: Number},
     TR_DESCRIPCION:{type:String},
     TR_CONVERSION_PZ: {type:Number},
-    TR_ESTATUS:{type: String}
+    TR_ESTATUS:{type: String},
+    TR_TIPO:{type: String}
 });
 
 module.exports = mongoose.model('tb_hh_traspasos', traspasoSchema);
